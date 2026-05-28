32cf00d8925c412f956f0624c0a0171d9e18dd63
Sakshi Singh <sakshi23042005@gmail.com>
2026-05-28 15:26:59 +0530

    Initial Commit

---
+++ tools.py
@@ -0,0 +1,40 @@
+from langchain.tools import tool
+import requests
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os
+from rich import print
+from dotenv import load_dotenv
+load_dotenv()
+
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def web_search(query: str)->str:
+    """Search the web for recent and reliable information on a topic. Returns Titles, URLs and snippets."""
+    results = tavily.search(query=query, max_results=3)
+    out = []
+    for r in results['results']:
+        out.append(f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n")
+
+    return "\n----\n".join(out)
+
+@tool
+def scrape_url(url: str)->str:
+    """Scrape and return clean text content from a given URL for deeper reading."""
+    try:
+        resp = requests.get(url, timeout=8, 
+                            headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/125.0.0.0 Safari/537.36"
+    )
+})
+        soup = BeautifulSoup(resp.text, "html.parser")
+        for tag in soup(["script", "style", "nav", "footer"]):
+            tag.decompose()
+        return soup.get_text(separator=" ", strip=True)[:300]
+    except Exception as e:
+        return f"Could not scrape URL: {str(e)}"
+
